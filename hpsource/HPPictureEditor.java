f57deaec931d90101f1d236456d2eb515e28adf0
lordizuriel <lordizuriel@93f66799-a940-0410-a959-217e56610a30>
2007-12-20 06:27:06 +0000

    Major update, added the key items to the editor frame and set up so that it loads and displays a selected picture.

--- hpsource/HPPictureEditor.java
+++ hpsource/HPPictureEditor.java
@@ -3,6 +3,9 @@ package hoipolloi;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.beans.*;
 /**
  * Hoi Polloi picture editor, mainly used to crop/resize pictures to fit the
  * size specified by the program for the profile picture.
@@ -24,6 +27,9 @@ public class HPPictureEditor extends JDialog implements ActionListener {
     // Buttons
     private JButton acceptButton = new JButton("Accept");
     private JButton cancelButton = new JButton("Cancel");
+    private JLabel imageLabel = new JLabel();
+    private BufferedImage currentImage = null;
+    private java.io.File lastSelectedFile = null;
     
     /**
      * Default constructor for the HPPictureEditor. This constructer sets up
@@ -37,14 +43,46 @@ public class HPPictureEditor extends JDialog implements ActionListener {
         this.setTitle("Picture Editor");
         this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         this.setModal(true);
+        this.setResizable(false);
         
         // JMenu
+            ImageIcon loadImageIcon = new ImageIcon(getClass().getClassLoader().getResource("picture.png"));
+            ImageIcon resizeImageIcon = new ImageIcon(getClass().getClassLoader().getResource("shape_handles.png"));
+            ImageIcon blankImageIcon = new ImageIcon(getClass().getClassLoader().getResource("blank.png"));
+            
             // File menu
             JMenu fileMenu = new JMenu("File");
             fileMenu.setMnemonic('F');
                 
-                JMenuItem loadImageItem = new JMenuItem(new MenuAction("Load Pic..."));
+            JMenuItem loadImageItem = new JMenuItem(new MenuAction("Open Picture"));
+            loadImageItem.setIcon(loadImageIcon);
+            loadImageItem.setMnemonic('O');
+            loadImageItem.setAccelerator(KeyStroke.getKeyStroke('O', KeyEvent.CTRL_DOWN_MASK));
+            
+            JMenuItem disposeItem = new JMenuItem(new MenuAction("Close"));
+            disposeItem.setIcon(blankImageIcon);
+            disposeItem.setAccelerator(KeyStroke.getKeyStroke('D', KeyEvent.CTRL_DOWN_MASK + KeyEvent.ALT_DOWN_MASK));
+            disposeItem.setMnemonic('C');
+            
+            fileMenu.add(loadImageItem);
+            fileMenu.addSeparator();
+            fileMenu.add(disposeItem);
+            
+            // Edit Menu
+            JMenu editMenu = new JMenu("Edit");
+            editMenu.setMnemonic('E');
+            
+            JMenuItem resizeImageItem = new JMenuItem(new MenuAction("Resize"));
+            resizeImageItem.setIcon(resizeImageIcon);
+            resizeImageItem.setMnemonic('R');
+            resizeImageItem.setAccelerator(KeyStroke.getKeyStroke('R', KeyEvent.ALT_DOWN_MASK));
                 
+            editMenu.add(resizeImageItem);
+                
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        this.setJMenuBar(menuBar);
         // End JMenu
         
         // Component set up
@@ -58,13 +96,19 @@ public class HPPictureEditor extends JDialog implements ActionListener {
         buttonPanel.add(acceptButton);
         buttonPanel.add(cancelButton);
         
+        JPanel imagePanel = new JPanel();
+        imagePanel.setLayout(new BorderLayout());
+        imagePanel.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.BLACK));
+        imagePanel.add(BorderLayout.CENTER, imageLabel);
+        
+        this.add(BorderLayout.CENTER, imagePanel);
         this.add(BorderLayout.SOUTH, buttonPanel);
         // End Frame Contents
         
         // Frame size and location
         
-        //this.setSize(100, 100);
-        this.pack();
+        this.setSize(400, 400);
+        //this.pack();
         
         // Set the location of the frame relative to the MainMenu
         // --CENTER--
@@ -100,7 +144,26 @@ public class HPPictureEditor extends JDialog implements ActionListener {
             this.dispose();
         }
     }
-
+    
+    public void resizeFrame() {
+        this.pack();
+        
+        // Set the location of the frame relative to the MainMenu
+        // --CENTER--
+        Point frameLocation = new Point();
+        
+        double frameX = parent.getLocation().getX() + ((parent.getWidth() / 2) - (this.getWidth() / 2));
+        double frameY = parent.getLocation().getY() + ((parent.getHeight() / 2) - (this.getHeight() / 2));
+        
+        frameLocation.setLocation(frameX, frameY);
+        this.setLocation(frameLocation);
+        // --END CENTER--
+    }
+    
+    public void disposeEditor() {
+        this.dispose();
+    }
+    
     class MenuAction extends AbstractAction {
         public MenuAction(String name) {
             super(name);
@@ -108,6 +171,131 @@ public class HPPictureEditor extends JDialog implements ActionListener {
         
         public void actionPerformed(ActionEvent evt) {
             String selection = (String)getValue(Action.NAME);
+            
+            if (selection.equals("Open Picture")) {
+                JFileChooser browser = new JFileChooser();
+                browser.setFileFilter(new ImageFileFilter());
+                browser.setAcceptAllFileFilterUsed(false);
+                ImagePreview preview = new ImagePreview(browser);
+                browser.addPropertyChangeListener(preview);
+                browser.setAccessory(preview);
+                
+                if (lastSelectedFile != null)
+                    browser.setSelectedFile(lastSelectedFile);
+                
+                int browserSelection = browser.showDialog(parent, "Open");
+            
+                if (browserSelection == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        lastSelectedFile = browser.getSelectedFile();
+                        currentImage = ImageIO.read(lastSelectedFile);
+                        imageLabel.setIcon(new ImageIcon(lastSelectedFile.getPath()));
+                        resizeFrame();
+                    } catch(java.io.IOException exc) {
+                        Debug.print("ERROR: Problem reading file in.");
+                    }
+                }
+            } else if (selection.equals("Close")) {
+                disposeEditor();
+            }
+        }
+    }
+    
+    class ImageFileFilter extends javax.swing.filechooser.FileFilter {
+        public ImageFileFilter() { super(); }
+        
+        public boolean accept(java.io.File file) {
+            String filePath = file.getPath();
+            String fileType = "";
+            if (!file.isDirectory()) {
+                if (filePath.length() > 5)
+                    fileType = filePath.substring(filePath.lastIndexOf('.'));
+                else
+                    return false;
+            }
+            //Debug.print("fileType: " + fileType);
+            boolean answer = false;
+            
+            if (fileType.equalsIgnoreCase(".jpg") ||
+                    fileType.equalsIgnoreCase(".jpeg") || 
+                    fileType.equalsIgnoreCase(".gif") || 
+                    fileType.equalsIgnoreCase(".png") || file.isDirectory())
+                answer = true;
+            
+            return answer;
+        }
+                
+        public String getDescription() {
+            return "Picture files (JPG, JPEG, PNG, GIF)";
+        }
+    }
+    /**
+     * Following code is Hack #31 from "Swing Hacks" by Chris Adamson and
+     * Joshua Marinacci. Minor edits include making the classes internal instead
+     * of separate files.
+     * 
+     * This adds a preview image to the JFileChooser so that the user may
+     * preview a thumbnail of the selected image instead of guessing or
+     * memorizing the names of the picture they are looking for.
+     * 
+     */
+    class ImagePreview extends JPanel implements PropertyChangeListener {
+        private JFileChooser jfc;
+        private Image img;
+
+        public ImagePreview(JFileChooser jfc) {
+            this.jfc = jfc;
+            Dimension sz = new Dimension(200,200);
+            setPreferredSize(sz);
+        }
+        
+        public void propertyChange(PropertyChangeEvent evt) {
+            try {
+                Debug.print("Updating.");
+                java.io.File file = jfc.getSelectedFile();
+                updateImage(file);
+            } catch (java.io.IOException ex) {
+                    Debug.print(ex.getMessage());
+            }
+        }
+        
+        public void updateImage(java.io.File file) throws java.io.IOException {
+            if(file == null) {
+                return;
+            }
+            
+            img = ImageIO.read(file);
+            repaint();
+        }
+        
+        public void paintComponent(Graphics g) {
+            // fill the background
+            g.setColor(Color.GRAY);
+            g.fillRect(0, 0, getWidth(), getHeight());
+        
+            if(img != null) {
+                // calculate the scaling factor
+                int imgW = img.getWidth(null);
+                int imgH = img.getHeight(null);
+                int side = Math.max(imgW, imgH);
+                double scale = 200.0 / (double)side;
+                int w = (int)(scale * (double)imgW);
+                int h = (int)(scale * (double)imgH);
+
+                // draw the image
+                g.drawImage(img, 0, 0, w, h, null);
+                
+                // draw the image dimensions
+                String dim = imgW + " x " + imgH;
+                g.setColor(Color.BLACK);
+                g.drawString(dim, 31, 196);
+                g.setColor(Color.WHITE);
+                g.drawString(dim, 30, 195);
+            } //else {
+                // print a message
+                //g.setColor(Color.BLACK);
+                //g.drawString("Not an image", 30, 100);
+            //}
         }
     }
 }
