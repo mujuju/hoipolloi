67f8c517748338af89df98111b405030ec3592ad
theletterpi <theletterpi@93f66799-a940-0410-a959-217e56610a30>
2009-01-04 19:00:48 +0000

    Removed unused imports.

--- hpsource/RubyTheme.java
+++ hpsource/RubyTheme.java
@@ -6,9 +6,6 @@ package hoipolloi;
 
 import javax.swing.plaf.*;
 import javax.swing.plaf.metal.*;
-import javax.swing.*;
-import javax.swing.border.*;
-import java.awt.*;
 
 /**
  * This class describes a theme using red colors.
